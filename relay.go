b45e1cfbb9aa0507895940a42f2dcc8619e0f7df
Sven Kirschbaum <sven@kirschbaum.me>
2025-05-03 03:46:47 +0200

    feat: Make remote EHLO/HELO configurable

--- relay.go
+++ relay.go
@@ -267,6 +267,62 @@ func (r *relay) recipientChecker(allowed, denied string) func(ctx context.Contex
 	}
 }
 
+// This function is mostly a copy of smtp.SendMail, with the addition of a helo parameter, to make is possible to change the default of localhost
+func sendMailWithHelo(addr string, helo string, a smtp.Auth, from string, to []string, msg []byte) error {
+	c, err := smtp.Dial(addr)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	if err = c.Hello(helo); err != nil {
+		return err
+	}
+
+	// We can't access c.serverName, so we need to extract the host from the addr, as it is done in smtp.Dial
+	host, _, _ := net.SplitHostPort(addr)
+
+	if ok, _ := c.Extension("STARTTLS"); ok {
+		config := &tls.Config{ServerName: host}
+		if err = c.StartTLS(config); err != nil {
+			return err
+		}
+	}
+
+	// We can't access c.ext directly
+	supportAuth, _ := c.Extension("AUTH")
+	if a != nil {
+		if !supportAuth {
+			return errors.New("smtp: server doesn't support AUTH")
+		}
+		if err = c.Auth(a); err != nil {
+			return err
+		}
+	}
+
+	if err = c.Mail(from); err != nil {
+		return err
+	}
+	for _, addr := range to {
+		if err = c.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(msg)
+	if err != nil {
+		return err
+	}
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+	return c.Quit()
+}
+
 func (r *relay) mailHandler(cfg *config) func(ctx context.Context, peer smtpd.Peer, env smtpd.Envelope) error {
 	return func(ctx context.Context, peer smtpd.Peer, env smtpd.Envelope) error {
 		// save upstream span as a link, we're going to re-parent this span to
@@ -336,8 +392,10 @@ func (r *relay) mailHandler(cfg *config) func(ctx context.Context, peer smtpd.Pe
 			observeDuration(ctx, statusCode, time.Since(start))
 		}()
 
-		err = smtp.SendMail(
+		// Use own implementation of sendMail to allow custom HELO
+		err = sendMailWithHelo(
 			cfg.remoteHost,
+			cfg.remoteHelo,
 			auth,
 			sender,
 			env.Recipients,
